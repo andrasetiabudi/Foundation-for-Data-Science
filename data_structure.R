c78e14bb2e143be696b42c8e15f2cb11f88accbc
andrasetiabudi <andraa.jp@gmail.com>
2020-03-30 16:50:54 +0700

    menambahkan data structure R

---
+++ data_structure.R
@@ -0,0 +1,11 @@
+names <- c("Adi","Budi","Cindi","Dedi")
+
+m <- matrix(1:12,nrow = 3)
+
+a <- array(1:24, c(3,4,2))
+
+users <- data.frame(
+  name = names,
+  gender = c("male","male","female","male"),
+  age = c(10,20,30,40)
+)
